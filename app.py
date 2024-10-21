86be0bebfac12c342570b6ccd81806fd0ca4c792
isamejiaavilaaa <isabella.mejia.estudio@gmail.com>
2024-10-20 21:13:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,55 @@
 import streamlit as st
 import cv2
 import numpy as np
-#from PIL import Image
-from PIL import Image as Image, ImageOps as ImagOps
+from PIL import Image as Image
 from keras.models import load_model
-
 import platform
 
 # Muestra la versión de Python junto con detalles adicionales
 st.write("Versión de Python:", platform.python_version())
 
+# Cargar el modelo entrenado
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
-#st.write("Versión de Python:", platform.python_version())
+st.subheader("Cierra y abre la mano para saber si el sistema lo detecta correctamente")
+
+# Mostrar imagen de referencia
 image = Image.open('OIG5.jpg')
 st.image(image, width=350)
+
+# Configuración de la barra lateral
 with st.sidebar:
-    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
+    st.subheader("Usando un modelo entrenado en Teachable Machine puedes usar esta app para identificar gestos de la mano.")
+
+# Captura de imagen desde la cámara
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-   #To read image file buffer as a PIL Image:
+    # Leer la imagen desde el buffer
     img = Image.open(img_file_buffer)
 
+    # Redimensionar la imagen
     newsize = (224, 224)
     img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
+
+    # Convertir la imagen a un array numpy
     img_array = np.array(img)
 
-    # Normalize the image
+    # Normalizar la imagen
     normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
+
+    # Cargar la imagen en el array
     data[0] = normalized_image_array
 
-    # run the inference
+    # Realizar la predicción
     prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Abierta, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('cerrada, con Probabilidad: '+str( prediction[0][1]))
-    #if prediction[0][2]>0.5:
-    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
-
+    # Mostrar el resultado de la predicción
+    if prediction[0][0] > 0.5:
+        st.header('Mano Abierta, con probabilidad: ' + str(prediction[0][0]))
+    elif prediction[0][1] > 0.5:
+        st.header('Mano Cerrada, con probabilidad: ' + str(prediction[0][1]))
+    else:
+        st.header('No se ha detectado la mano correctamente.')
